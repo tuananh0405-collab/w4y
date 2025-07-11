68290af4ba049033efafb75d6c9d3c28740a2558
Ngoc Dinh <ngocdbhe182383@fpt.edu.vn>
2025-07-12 00:25:09 +0700

    feat: admin dashboard user management, add attribute to user model please notice this change, add new user list api, user stats, authorize admin, add buildFilter to handle filter user table

--- routes/job.routes.js
+++ routes/job.routes.js
@@ -39,7 +39,7 @@ import {
   validateUpdateJobStatus,
   validateUpdateApplicationStatus,
 } from "../controllers/job.controller.js";
-import { authenticate } from "../middlewares/auth.middleware.js";
+import { authenticate, authorizeAdmin } from "../middlewares/auth.middleware.js";
 
 const jobRouter = Router();
 
@@ -56,22 +56,21 @@ jobRouter.delete("/delete/:jobId", authenticate, deleteJob);
  * Get monthly job statistics for the admin dashboard
  * Allowed Roles: Admin
  */
-//admin athen required
-jobRouter.get("/stats/monthly", getMonthlyJobStats);
+jobRouter.get("/stats/monthly",authenticate, authorizeAdmin, getMonthlyJobStats);
 
 /**
  * GET /api/v1/job/stats/quarterly
  * Get quarterly job statistics for the admin dashboard
  * Allowed Roles: Admin
  */ 
-jobRouter.get("/stats/quarterly", getQuarterlyJobStats);
+jobRouter.get("/stats/quarterly",authenticate, authorizeAdmin, getQuarterlyJobStats);
 
 /**
  * GET /api/v1/job/stats/yearly
  * Get yearly job statistics for the admin dashboard
  * Allowed Roles: Admin
  */
-jobRouter.get("/stats/yearly", getYearlyJobStats);
+jobRouter.get("/stats/yearly",authenticate, authorizeAdmin, getYearlyJobStats);
 
 // ===== BASIC CRUD OPERATIONS =====
 
@@ -94,8 +93,7 @@ jobRouter.get("/", viewJobList);
  * Get an overview of job posts
  * Allowed Roles: Admin
  */
-//authenticate required
-jobRouter.get("/overview", getJobOverview);
+jobRouter.get("/overview",authenticate, authorizeAdmin, getJobOverview);
 
 /**
  * GET /api/v1/job/recommended
@@ -153,7 +151,7 @@ jobRouter.put("/:id/hide", authenticate, hideJob);
  * Change the status of a job
  * Allowed Roles: Admin
  */
-jobRouter.patch("/:id/status", authenticate, validateUpdateJobStatus, updateJobStatus);
+jobRouter.patch("/:id/status", authenticate, authorizeAdmin, validateUpdateJobStatus, updateJobStatus);
 
 /**
  * POST /api/v1/job/:id/view
@@ -200,8 +198,5 @@ jobRouter.patch("/:id/applications/:applicantId", authenticate, validateUpdateAp
  * Allowed Roles: All
  */
 jobRouter.get("/related/:id", getRelatedJobs);
-
-
-
 
 export default jobRouter;
